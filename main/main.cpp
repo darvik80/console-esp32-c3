68242dc975785b9aa2bdd5d1a43a7defe1413e40
Ivan Kishchenko <ivan.kishchenko@lazada.com>
2024-07-23 22:22:35 +0700

    Init

--- main/main.cpp
+++ main/main.cpp
@@ -1,29 +1,34 @@
 #include <esp_heap_caps_init.h>
 
 #include <core/Core.h>
+#include <core/system/storage/NvsStorage.h>
 #include <core/system/telemetry/TelemetryService.h>
 #include <core/system/wifi/WifiService.h>
 #include <core/system/mqtt/MqttService.h>
 #include <core/system/console/Console.h>
+#include <soc/gpio_num.h>
+#include <driver/gpio.h>
+#include "core/system/exchange/UartExchange.h"
 
 class Console : public Application<Console> {
 public:
     Console() = default;
 protected:
     void userSetup() override {
-        getRegistry().getEventBus().subscribe(shared_from_this());
-        getRegistry().create<TelemetryService>();
-        getRegistry().create<WifiService>();
-        auto &mqtt = getRegistry().create<MqttService>();
-        mqtt.addJsonProcessor<Telemetry>("/user/telemetry");
-
+        getRegistry().create<NvsStorage>();
         getRegistry().create<UartConsoleService>();
+        getRegistry().create<UartExchange>();
+        getRegistry().create<WifiService>();
+//        getRegistry().create<TelemetryService>();
+//        auto &mqtt = getRegistry().create<MqttService>();
+//        mqtt.addJsonProcessor<Telemetry>("/user/telemetry");
     }
 };
 
 static std::shared_ptr<Console> app;
 
 extern "C" void app_main() {
+    // esp_log_level_set("*", ESP_LOG_DEBUG);
     size_t free = heap_caps_get_free_size(MALLOC_CAP_DEFAULT);
     size_t total = heap_caps_get_total_size(MALLOC_CAP_DEFAULT);
     esp_logi(app, "heap: %zu/%zu", free, total);
