c5c028eb0ac03d14973abef4a04da85ac4e77df7
Ivan Kishchenko <ivan.kishchenko@lazada.com>
2024-11-01 16:17:21 +0700

    esp-now support cleanup

--- main/main.cpp
+++ main/main.cpp
@@ -5,33 +5,88 @@
 #include <core/system/wifi/WifiService.h>
 #include <core/system/mqtt/MqttService.h>
 #include <exchange/espnow/EspNowExchange.h>
-#include "led/LedStripService.h"
-class Console : public Application<Console> {
+
+#ifdef CONFIG_EXCHANGE_BUS_SPI
+#include <exchange/spi/SpiExchange.h>
+#endif
+
+#ifdef CONFIG_EXCHANGE_BUS_UART
+#include <exchange/uart/UartExchange.h>
+#endif
+
+
+class Console : public Application<Console>,
+                public TMessageSubscriber<Console, ExchangeMessage, TimerEvent<100>, TimerEvent<101> > {
+    FreeRTOSTimer _timerEspNow;
+    FreeRTOSTimer _timerUart;
+
 public:
     Console() = default;
 
+    void handle(const TimerEvent<100> &event) {
+        std::string ping = "ping " + std::to_string(esp_timer_get_time());
+        exchange_message_t buf{
+            .if_type = ESP_INTERNAL_IF,
+            .if_num = 0x08,
+            .pkt_type = PACKET_TYPE_COMMAND_REQUEST,
+            .payload = (void *) ping.data()
+        };
+        buf.length = ping.size() + 1;
+        esp_logi(esp_now_exchange, "Send: %s", ping.c_str());
+        getRegistry().getService<EspNowExchange>()->send(buf);
+    }
+
+    void handle(const TimerEvent<101> &event) {
+        std::string ping = "ping " + std::to_string(esp_timer_get_time());
+        exchange_message_t buf{
+            .if_type = ESP_INTERNAL_IF,
+            .if_num = 0x08,
+            .pkt_type = PACKET_TYPE_COMMAND_REQUEST,
+            .payload = (void *) ping.data()
+        };
+        buf.length = ping.size() + 1;
+        esp_logi(uart_exchange, "Send: %s", ping.c_str());
+        getRegistry().getService<UartExchange>()->send(buf);
+    }
+
+    void handle(const ExchangeMessage &event) {
+        const auto &msg = event.message;
+        std::string_view ping((char *) (msg.payload + msg.hdr->offset), msg.hdr->payload_len);
+        esp_logi(console, "Recv: %s, %d", ping.data(), ping.length());
+        if (ping.starts_with("ping")) {
+            std::string pong("pong");
+            pong += ping.substr(4);
+            exchange_message_t buf{
+                .if_type = ESP_INTERNAL_IF,
+                .if_num = 0x02,
+                .pkt_type = PACKET_TYPE_COMMAND_RESPONSE,
+            };
+            buf.payload = (void *) pong.data();
+            buf.length = pong.length() + 1;
+
+            esp_logi(console, "Send: %s, %d", pong.c_str(), pong.length());
+
+            event.exchange.send(buf);
+        }
+    }
+
 protected:
     void userSetup() override {
-        ///getRegistry().create<NvsStorage>();
-        //getRegistry().create<UartConsoleService>();
-        //getRegistry().create<UartExchange>();
-        //getRegistry().create<SpiExchange>();
-        //getRegistry().create<I2cExchange>();
+        getRegistry().getEventBus().subscribe(shared_from_this());
+#ifdef CONFIG_EXCHANGE_BUS_SPI
+        getRegistry().create<SpiExchange>();
+#endif
+#ifdef CONFIG_EXCHANGE_BUS_UART
+        getRegistry().create<UartExchange>();
+        _timerUart.fire<101>(10000, true);
+#endif
+#ifdef CONFIG_EXCHANGE_BUS_I2C
+        getRegistry().create<I2cExchange>();
+#endif
+#ifdef CONFIG_EXCHANGE_BUS_ESP_NOW
         getRegistry().create<EspNowExchange>();
-        //getRegistry().create<WifiService>();
-
-        // auto &mqtt = getRegistry().create<MqttService>();
-        // mqtt.addJsonProcessor<Telemetry>("/user/telemetry");
-
-        //getRegistry().create<TelemetryService>();
-        // auto &led1 = getRegistry().create<LedStripService<Service_App_LedStrip1, 17, 8> >();
-        // auto &led2 = getRegistry().create<LedStripService<Service_App_LedStrip2, 16, 8> >();
-        //
-        //
-        // led1.setColor(0, 7, LedColor{255, 255, 255});
-        // led1.refresh();
-        // led2.setColor(0, 7, LedColor{0, 255, 0});
-        // led2.refresh();
+        _timerEspNow.fire<100>(10000, true);
+#endif
     }
 };
 
